c816c45522fec53b6ed865ff57970ca155a59763
Pedrag1234 <pedrag1234@gmail.com>
2019-05-18 16:33:49 +0100

    added files for generating trees

---
+++ src/compound-objects/MyLSPlant.js
@@ -0,0 +1,14 @@
+class MyLSPlant extends MyLSystem{
+
+    constructor(scene){
+        super(scene);
+        this.scene = scene;
+    }
+
+    initGrammar() {
+        this.grammar = {
+            "F": new MyBranch(this.scene),
+            "X": new MyLeaf(this.scene)
+        }
+    }
+}
---
+++ src/compound-objects/MyLeaf.js
@@ -0,0 +1,20 @@
+class MyLeaf extends MyDiamond{
+    constructor(scene) {
+        super(scene);
+        this.scene = scene;
+        this.leafMaterial = new CGFappearance(scene);
+        this.leafMaterial.setAmbient(35/255,138/255,34/255,1);
+        this.leafMaterial.setDiffuse(35/255,138/255,34/255,1);
+        this.leafMaterial.setSpecular(35/255,138/255,34/255,1);
+        this.leafMaterial.setShininess(50);
+    }
+
+    display() {
+        this.leafMaterial.apply();
+        this.scene.pushMatrix();
+        {
+            super.display();
+        }
+        this.scene.popMatrix();
+    }
+}
