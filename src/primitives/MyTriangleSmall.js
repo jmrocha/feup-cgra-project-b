5b9d9d2c616b0e4da274307b1b7fc764af646b86
João Rocha <rocha.jmiguel@gmail.com>
2019-05-19 18:36:13 +0100

    Add small triangle primitive

---
+++ src/primitives/MyTriangleSmall.js
@@ -0,0 +1,25 @@
+class MyTriangleSmall extends CGFobject {
+    constructor(scene, texCoords) {
+        super(scene);
+        this.texCoords = texCoords;
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.vertices = [
+            -1, 0, 0,	//0
+            0, 1, 0,	//1
+            1, 0, 0,	//2
+            0, 0, 0	    //3
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 3, 1,
+            1, 3, 2
+        ];
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
+
+export default MyTriangleSmall;
