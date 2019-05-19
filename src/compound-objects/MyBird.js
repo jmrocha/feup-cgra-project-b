fb5f0c5560780939eaaf08ab8eb0b452b940db8b
João Rocha <rocha.jmiguel@gmail.com>
2019-05-19 18:36:52 +0100

    Add bird's body and wings

--- src/compound-objects/MyBird.js
+++ src/compound-objects/MyBird.js
@@ -1,6 +1,12 @@
+import {MyUnitCubeQuad} from "../primitives/MyUnitCubeQuad.js";
+import MyTriangleSmall from "../primitives/MyTriangleSmall.js";
+import Utils from "../Utils.js";
+
 class MyBird extends CGFobject {
     constructor(scene) {
         super(scene);
+        this.body = new MyUnitCubeQuad(scene);
+        this.wing = new MyTriangleSmall(scene);
     }
 
     display() {
@@ -9,7 +15,7 @@ class MyBird extends CGFobject {
     }
 
     displayBody() {
-
+        this.body.display();
     }
 
     displayWings() {
@@ -18,11 +24,26 @@ class MyBird extends CGFobject {
     }
 
     displayWingLeft() {
-
+        this.scene.pushMatrix();
+        {
+            let angle = Utils.degToRad(90);
+            this.scene.rotate(angle, 1, 0, 0);
+            this.scene.rotate(angle, 0, 0, 1);
+            this.wing.display();
+        }
+        this.scene.popMatrix();
     }
 
     displayWingRight() {
-
+        this.scene.pushMatrix();
+        {
+            let alpha = Utils.degToRad(90);
+            let beta = Utils.degToRad(-90);
+            this.scene.rotate(alpha, 1, 0, 0);
+            this.scene.rotate(beta, 0, 0, 1);
+            this.wing.display();
+        }
+        this.scene.popMatrix();
     }
 
     update(t) {
