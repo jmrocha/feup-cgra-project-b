f93493411a2d6310a1b73e09c6671473131c8505
João Rocha <rocha.jmiguel@gmail.com>
2019-05-28 18:44:44 +0100

    Add pos/orientation to MyTreeBranch

--- src/compound-objects/MyTreeBranch.js
+++ src/compound-objects/MyTreeBranch.js
@@ -4,9 +4,12 @@ import Utils from "../Utils.js";
 const BRANCH_SLICES = 50;
 
 class MyTreeBranch extends CGFobject {
-    constructor(scene) {
+    constructor(scene, position = [0, 0, 0], orientation = 0) {
         super(scene);
         this.branch = new MyCylinder(scene, BRANCH_SLICES);
+
+        this.position = position;
+        this.orientation = Utils.degToRad(orientation);
 
         this.material = new CGFappearance(scene);
         this.material.setAmbient(0.2, 0.4, 0.8, 1.0);
@@ -24,13 +27,19 @@ class MyTreeBranch extends CGFobject {
 
         this.scene.pushMatrix();
         {
-            let angle = Utils.degToRad(90);
-            this.scene.scale(...this.scale);
-            this.scene.translate(0, this.branchTY, 0);
-            this.scene.rotate(angle, 1, 0, 0);
+            this.scene.rotate(this.orientation, 0, 1, 0);
+            this.setDefaultPosition();
+            this.scene.translate(this.position[0], this.position[1], this.position[2]);
             this.branch.display();
         }
         this.scene.popMatrix();
+    }
+
+    setDefaultPosition() {
+        let angle = Utils.degToRad(90);
+        this.scene.scale(...this.scale);
+        this.scene.translate(0, this.branchTY, 0);
+        this.scene.rotate(angle, 1, 0, 0);
     }
 }
 
