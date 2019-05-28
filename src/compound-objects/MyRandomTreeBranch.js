db3ba98c5e28c5b3f1ddda62320c17fb9e562755
João Rocha <rocha.jmiguel@gmail.com>
2019-05-28 18:45:29 +0100

    Add MyRandomTreeBranch

---
+++ src/compound-objects/MyRandomTreeBranch.js
@@ -0,0 +1,26 @@
+import Utils from "../Utils.js";
+import MyTreeBranch from "./MyTreeBranch.js";
+
+const BRANCH_SLICES = 50;
+
+class MyRandomTreeBranch extends MyTreeBranch {
+    constructor(scene) {
+        super(scene);
+        this.position = this.getRandomPosition();
+        this.orientation = this.getRandomOrientation();
+    }
+
+    getRandomPosition() {
+        let randomX = Utils.getRand(0, 3);
+        let randomY = Utils.getRand(0, 3);
+        let randomZ = Utils.getRand(0, 3);
+
+        return [randomX, randomY, randomZ];
+    }
+
+    getRandomOrientation() {
+        return Utils.getRand(0, 360);
+    }
+}
+
+export default MyRandomTreeBranch;
