53ccb780eab9adf1373f155056c19792c9cd99b7
João Rocha <rocha.jmiguel@gmail.com>
2019-05-30 14:44:41 +0100

    Add position to Nest

--- src/compound-objects/MyNest.js
+++ src/compound-objects/MyNest.js
@@ -2,8 +2,9 @@ import Plane from "../Plane.js";
 import Utils from "../Utils.js";
 
 class MyNest extends CGFobject {
-    constructor(scene) {
+    constructor(scene, position = [0, 0, 0]) {
         super(scene);
+        this.position = position;
         this.nest = new Plane(scene);
     }
 
@@ -11,6 +12,7 @@ class MyNest extends CGFobject {
         this.scene.pushMatrix();
         {
             let angle = Utils.degToRad(-90);
+            this.scene.translate(this.position[0], this.position[1], this.position[2]);
             this.scene.rotate(angle, 1, 0, 0);
             this.nest.display();
         }
