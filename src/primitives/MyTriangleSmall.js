fbcd2ee344e8d7a2207580f7b40eec1a1e52d2fc
João Rocha <rocha.jmiguel@gmail.com>
2019-05-30 18:41:40 +0100

    Make triangle unitary, centered at (0, 0, 0)

--- src/primitives/MyTriangleSmall.js
+++ src/primitives/MyTriangleSmall.js
@@ -7,9 +7,9 @@ class MyTriangleSmall extends CGFobject {
 
     initBuffers() {
         this.vertices = [
-            -1, 0, 0,	//0
-            0, 1, 0,	//1
-            1, 0, 0,	//2
+            -0.5, 0, 0,	//0
+            0, 0.5, 0,	//1
+            0.5, 0, 0,	//2
             0, 0, 0	    //3
         ];
 
