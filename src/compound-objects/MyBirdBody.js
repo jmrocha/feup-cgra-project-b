0bca9ec11a0d6e56d19a0a43404eb75ccf6df771
João Rocha <rocha.jmiguel@gmail.com>
2019-05-31 10:15:52 +0100

    Rename eye to body

--- src/compound-objects/MyBirdBody.js
+++ src/compound-objects/MyBirdBody.js
@@ -3,14 +3,14 @@ import MyUnitCubeQuad from "./MyUnitCubeQuad.js";
 class MyBirdBody extends CGFobject {
     constructor(scene) {
         super(scene);
-        this.eye = new MyUnitCubeQuad(scene);
+        this.body = new MyUnitCubeQuad(scene);
         this.material = new CGFappearance(scene);
         this.material.setDiffuse(0.3, 0.3, 0.3, 1);
     }
 
     display() {
         this.material.apply();
-        this.eye.display();
+        this.body.display();
     }
 }
 
