8fa5bd287cfcdfb1260362f5b2794ca695bd11be
João Rocha <rocha.jmiguel@gmail.com>
2019-06-01 16:00:54 +0100

    Make Nest as a cube

--- src/compound-objects/MyNest.js
+++ src/compound-objects/MyNest.js
@@ -1,11 +1,12 @@
 import Plane from "../Plane.js";
 import Utils from "../Utils.js";
+import MyUnitCubeQuad from "./MyUnitCubeQuad.js";
 
 class MyNest extends CGFobject {
     constructor(scene, position = [0, 0, 0]) {
         super(scene);
         this.position = position;
-        this.nest = new Plane(scene);
+        this.nest = new MyUnitCubeQuad(scene);
         this.material = new CGFappearance(scene);
     }
 
